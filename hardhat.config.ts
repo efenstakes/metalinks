889ec0aca7bd94353d20309ae242c37156fb5892
Efen Stakes <efenstakes101@gmail.com>
2023-05-16 12:49:28 +0300

    update hardhat config to add sepolia testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,54 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+// import { HardhatUserConfig } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+// const config: HardhatUserConfig = {
+//   solidity: "0.8.18",
+// };
+
+// export default config;
+
+require("dotenv").config();
+// import { task } from 'hardhat'
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs: any, hre: any) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_URL,
+      accounts: { mnemonic: process.env.MNEMONIC },
+    },
+    polygon_mumbai: {
+      url: process.env.ALCHEMY_URL,
+      accounts: { mnemonic: process.env.MNEMONIC },
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: `${process.env.POLYGONSCAN_API_KEY}`
+    },
+  },
 };
-
-export default config;
